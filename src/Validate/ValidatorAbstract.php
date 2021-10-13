f44acb489f8ec2354e2cccb032255be0bcd8b3e3
Oliver Etchebarne <yo@drmad.org>
2021-10-13 01:08:38 -0500

    Add method setForm() to validations

--- src/Validate/ValidatorAbstract.php
+++ src/Validate/ValidatorAbstract.php
@@ -1,16 +1,25 @@
 <?php
 namespace Vendimia\Form\Validate;
 
+use Vendimia\Form\Form;
+
 abstract class ValidatorAbstract implements ValidatorInterface
 {
     protected array $messages = [];
     protected array $fail_templates = [];
+    protected Form $form;
 
-    public function __construct(?array $fail_templates = null) 
+    public function __construct(
+        ?array $fail_templates = null)
     {
         if ($fail_templates) {
             $this->fail_templates = $fail_templates;
         }
+    }
+
+    public function setForm(Form $form): void
+    {
+        $this->form = $form;
     }
 
     public function getMessages(): array
@@ -18,7 +27,7 @@ abstract class ValidatorAbstract implements ValidatorInterface
         return $this->messages;
     }
 
-    /** 
+    /**
      * Adds a message, replacing variables
      */
     public function addMessage($code, ...$args)
--- src/Validate/ValidatorInterface.php
+++ src/Validate/ValidatorInterface.php
@@ -1,7 +1,11 @@
 <?php
 namespace Vendimia\Form\Validate;
 
+use Vendimia\Form\Form;
+
 interface ValidatorInterface
 {
+    public function setForm(Form $form): void;
+
     public function validate($value): bool;
 }
